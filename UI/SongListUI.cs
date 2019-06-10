17da49a29c2a003ece21fb20e2240dcb3dfa1e78
Chris <chrislee0419@gmail.com>
2019-06-10 22:19:07 +0100

    minor update to target version still not buildable

--- UI/SongListUI.cs
+++ UI/SongListUI.cs
@@ -349,7 +349,7 @@ namespace EnhancedSearchAndFilters.UI
                 _levelsTableView.SelectCellWithIdx(row);
             }
 
-            _levelsViewController.HandleLevelPackLevelsTableViewDidSelectLevel(null, level);
+            _levelsViewController.InvokePrivateMethod("HandleLevelPackLevelsTableViewDidSelectLevel", new object[] { null, level });
         }
     }
 }
