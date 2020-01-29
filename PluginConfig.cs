c46740be2a00ad0b847d4906ef41706ed2186db7
Chris <chrislee0419@gmail.com>
2020-01-29 21:48:35 +0000

    refactors PluginConfig.cs

--- PluginConfig.cs
+++ PluginConfig.cs
@@ -5,22 +5,20 @@ namespace EnhancedSearchAndFilters
 {
     internal static class PluginConfig
     {
-        static private readonly Config config = new Config("EnhancedSearchAndFilters");
+        private static readonly Config config = new Config("EnhancedSearchAndFilters");
+        private const string MainSection = "EnhancedSearchAndFilters";
 
         /// <summary>
         /// The maximum number of songs that can be displayed as an intermediate search result.
         /// </summary>
-        static public int MaxSearchResults
+        public static int MaxSearchResults
         {
             get
             {
-                int value = config.GetInt("EnhancedSearchAndFilters", "MaxSearchResults", MaxSearchResultsDefaultValue, true);
+                int value = config.GetInt(MainSection, "MaxSearchResults", MaxSearchResultsDefaultValue, true);
                 return Math.Min(Math.Max(value, MaxSearchResultsMinValue), MaxSearchResultsUnlimitedValue);
             }
-            set
-            {
-                config.SetInt("EnhancedSearchAndFilters", "MaxSearchResults", value);
-            }
+            set => config.SetInt(MainSection, "MaxSearchResults", value);
         }
         public const int MaxSearchResultsDefaultValue = 14;
         public const int MaxSearchResultsMaxValue = 28;
@@ -31,113 +29,71 @@ namespace EnhancedSearchAndFilters
         /// <summary>
         /// Determine whether to strip symbols from song details when performing a search
         /// </summary>
-        static public bool StripSymbols
+        public static bool StripSymbols
         {
-            get
-            {
-                return config.GetBool("EnhancedSearchAndFilters", "StripSymbolsFromSearch", StripSymbolsDefaultValue, true);
-            }
-            set
-            {
-                config.SetBool("EnhancedSearchAndFilters", "StripSymbolsFromSearch", value);
-            }
+            get => config.GetBool(MainSection, "StripSymbolsFromSearch", StripSymbolsDefaultValue, true);
+            set => config.SetBool(MainSection, "StripSymbolsFromSearch", value);
         }
         public const bool StripSymbolsDefaultValue = true;
 
         /// <summary>
         /// Determine whether to query each word independently (A word is any cluster of characters separated by a space)
         /// </summary>
-        static public bool SplitQueryByWords
+        public static bool SplitQueryByWords
         {
-            get
-            {
-                return config.GetBool("EnhancedSearchAndFilters", "SplitQueryByWords", SplitQueryByWordsDefaultValue, true);
-            }
-            set
-            {
-                config.SetBool("EnhancedSearchAndFilters", "SplitQueryByWords", value);
-            }
+            get => config.GetBool(MainSection, "SplitQueryByWords", SplitQueryByWordsDefaultValue, true);
+            set => config.SetBool(MainSection, "SplitQueryByWords", value);
         }
         public const bool SplitQueryByWordsDefaultValue = true;
 
         /// <summary>
         /// Determine which parts of a beatmap's details to search
         /// </summary>
-        static public SearchableSongFields SongFieldsToSearch
+        public static SearchableSongFields SongFieldsToSearch
         {
             get
             {
-                int value = config.GetInt("EnhancedSearchAndFilters", "SongFieldsToSearch", (int)SongFieldsToSearchDefaultValue, true);
+                int value = config.GetInt(MainSection, "SongFieldsToSearch", (int)SongFieldsToSearchDefaultValue, true);
                 return Enum.IsDefined(typeof(SearchableSongFields), value) ? (SearchableSongFields)value : SongFieldsToSearchDefaultValue;
             }
-            set
-            {
-                config.SetInt("EnhancedSearchAndFilters", "SongFieldsToSearch", (int)value);
-            }
+            set => config.SetInt(MainSection, "SongFieldsToSearch", (int)value);
         }
         public const SearchableSongFields SongFieldsToSearchDefaultValue = SearchableSongFields.All;
 
         /// <summary>
         /// Use the compact, single-screen keyboard in the search screen.
         /// </summary>
-        static public bool CompactSearchMode
+        public static bool CompactSearchMode
         {
-            get
-            {
-                return config.GetBool("EnhancedSearchAndFilters", "CompactSearchMode", CompactSearchModeDefaultValue, true);
-            }
-            set
-            {
-                config.SetBool("EnhancedSearchAndFilters", "CompactSearchMode", value);
-            }
+            get => config.GetBool(MainSection, "CompactSearchMode", CompactSearchModeDefaultValue, true);
+            set => config.SetBool(MainSection, "CompactSearchMode", value);
         }
         public const bool CompactSearchModeDefaultValue = false;
 
         /// <summary>
         /// The number of songs to search through in one frame. This setting is not exposed in the UI and can only be edited in the config.
         /// </summary>
-        static public int MaxSongsToSearchInOneFrame
+        public static int MaxSongsToSearchInOneFrame
         {
-            get
-            {
-                return config.GetInt("EnhancedSearchAndFilters", "MaxSongsToSearchInOneFrame", 100, true);
-            }
+            get => config.GetInt(MainSection, "MaxSongsToSearchInOneFrame", 100, true);
         }
 
-        static public bool ShowFirstTimeLoadingText
+        public static bool ShowFirstTimeLoadingText
         {
-            get
-            {
-                return config.GetBool("EnhancedSearchAndFilters", "ShowFirstTimeLoadingText", true, true);
-            }
-            set
-            {
-                config.SetBool("EnhancedSearchAndFilters", "ShowFirstTimeLoadingText", value);
-            }
+            get => config.GetBool(MainSection, "ShowFirstTimeLoadingText", true, true);
+            set => config.SetBool(MainSection, "ShowFirstTimeLoadingText", value);
         }
 
         public static bool DisableSearch
         {
-            get
-            {
-                return config.GetBool("EnhancedSearchAndFilters", "DisableSearch", false, true);
-            }
-            set
-            {
-                config.SetBool("EnhancedSearchAndFilters", "DisableSearch", value);
-            }
+            get => config.GetBool(MainSection, "DisableSearch", false, true);
+            set => config.SetBool(MainSection, "DisableSearch", value);
         }
 
         public static bool DisableFilters
         {
-            get
-            {
-                return config.GetBool("EnhancedSearchAndFilters", "DisableFilter", false, true);
-            }
-            set
-            {
-                config.SetBool("EnhancedSearchAndFilters", "DisableFilter", value);
-            }
+            get => config.GetBool(MainSection, "DisableFilter", false, true);
+            set => config.SetBool(MainSection, "DisableFilter", value);
         }
     }
 
