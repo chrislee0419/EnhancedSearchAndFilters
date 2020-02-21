97f554c667dc01ebf835062e1462cae03fe11964
Chris <chrislee0419@gmail.com>
2020-02-21 00:02:03 +0000

    adds more tutorial pages
    - also, tweaks some of the existing pages

--- UI/Components/FilterTutorialPages.cs
+++ UI/Components/FilterTutorialPages.cs
@@ -59,7 +59,7 @@ namespace EnhancedSearchAndFilters.UI.Components
             }
         }
         private bool _page2Active = false;
-        [UIValue("quick-filter-page-active")]
+        [UIValue("filter-loading-page-active")]
         public bool Page2Active
         {
             get => _page2Active;
@@ -74,7 +74,7 @@ namespace EnhancedSearchAndFilters.UI.Components
             }
         }
         private bool _page3Active = false;
-        [UIValue("quick-filter-page-2-active")]
+        [UIValue("disable-filter-page-active")]
         public bool Page3Active
         {
             get => _page3Active;
@@ -88,6 +88,56 @@ namespace EnhancedSearchAndFilters.UI.Components
                 NotifyPropertyChanged();
             }
         }
+        private bool _page4Active = false;
+        [UIValue("quick-filter-page-active")]
+        public bool Page4Active
+        {
+            get => _page4Active;
+            set
+            {
+                if (_page4Active == value)
+                    return;
+
+                _page4Active = value;
+                _indicator4.color = value ? SelectedPageIndicatorColor : DefaultPageIndicatorColor;
+                NotifyPropertyChanged();
+            }
+        }
+        private bool _page5Active = false;
+        [UIValue("quick-filter-page-2-active")]
+        public bool Page5Active
+        {
+            get => _page5Active;
+            set
+            {
+                if (_page5Active == value)
+                    return;
+
+                _page5Active = value;
+                _indicator5.color = value ? SelectedPageIndicatorColor : DefaultPageIndicatorColor;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private int _currentPage = 1;
+        private int CurrentPage
+        {
+            get => _currentPage;
+            set
+            {
+                value = value < 1 ? 1 : value;
+                _currentPage = value > NumberOfPages ? NumberOfPages : value;
+
+                Page1Active = _currentPage == 1;
+                Page2Active = _currentPage == 2;
+                Page3Active = _currentPage == 3;
+                Page4Active = _currentPage == 4;
+                Page5Active = _currentPage == 5;
+
+                PreviousButtonActive = _currentPage > 1;
+                NextButtonActive = _currentPage < NumberOfPages;
+            }
+        }
 
 #pragma warning disable CS0649
         [UIComponent("indicator-1")]
@@ -96,6 +146,10 @@ namespace EnhancedSearchAndFilters.UI.Components
         private RawImage _indicator2;
         [UIComponent("indicator-3")]
         private RawImage _indicator3;
+        [UIComponent("indicator-4")]
+        private RawImage _indicator4;
+        [UIComponent("indicator-5")]
+        private RawImage _indicator5;
 
         [UIObject("filter-list-example-image")]
         private GameObject _filterListExampleImageGO;
@@ -112,13 +166,18 @@ namespace EnhancedSearchAndFilters.UI.Components
 
         private static readonly Vector2 OutlineEffectDistance = new Vector2(0.3f, 0.3f);
 
+        private const int NumberOfPages = 5;
+
         private void Start()
         {
             BSMLParser.instance.Parse(BSMLUtilities.GetResourceContent(Assembly.GetExecutingAssembly(), "EnhancedSearchAndFilters.UI.Views.Filters.FilterTutorialView.bsml"), this.gameObject, this);
 
+            _currentPage = 1;
             _indicator1.color = SelectedPageIndicatorColor;
             _indicator2.color = DefaultPageIndicatorColor;
             _indicator3.color = DefaultPageIndicatorColor;
+            _indicator4.color = DefaultPageIndicatorColor;
+            _indicator5.color = DefaultPageIndicatorColor;
 
             // add an outline to all images
             var outline = _filterListExampleImageGO.AddComponent<Outline>();
@@ -139,15 +198,8 @@ namespace EnhancedSearchAndFilters.UI.Components
         /// </summary>
         public void ResetPages()
         {
-            if (!_isInitialized)
-                return;
-
-            Page1Active = true;
-            Page2Active = false;
-            Page3Active = false;
-
-            PreviousButtonActive = false;
-            NextButtonActive = true;
+            if (_isInitialized)
+                CurrentPage = 1;
         }
 
         #region BSML Actions
@@ -155,50 +207,10 @@ namespace EnhancedSearchAndFilters.UI.Components
         private void OnCloseButtonClicked() => CloseButtonPressed?.Invoke();
 
         [UIAction("previous-button-clicked")]
-        private void OnPreviousButtonClicked()
-        {
-            if (Page3Active)
-            {
-                Page1Active = false;
-                Page2Active = true;
-                Page3Active = false;
-
-                PreviousButtonActive = true;
-                NextButtonActive = true;
-            }
-            else
-            {
-                Page1Active = true;
-                Page2Active = false;
-                Page3Active = false;
-
-                PreviousButtonActive = false;
-                NextButtonActive = true;
-            }
-        }
+        private void OnPreviousButtonClicked() => CurrentPage -= 1;
 
         [UIAction("next-button-clicked")]
-        private void OnNextButtonClicked()
-        {
-            if (Page1Active)
-            {
-                Page1Active = false;
-                Page2Active = true;
-                Page3Active = false;
-
-                PreviousButtonActive = true;
-                NextButtonActive = true;
-            }
-            else
-            {
-                Page1Active = false;
-                Page2Active = false;
-                Page3Active = true;
-
-                PreviousButtonActive = true;
-                NextButtonActive = false;
-            }
-        }
+        private void OnNextButtonClicked() => CurrentPage += 1;
         #endregion
 
         private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
