4984d00f85dccbf45348a7b12b12c14a34521c31
Chris <chrislee0419@gmail.com>
2020-05-05 21:16:42 +0100

    changes search filter keyboard layout

--- UI/Components/CompactSearchKeyboard.cs
+++ UI/Components/CompactSearchKeyboard.cs
@@ -9,10 +9,9 @@ namespace EnhancedSearchAndFilters.UI.Components
 {
     internal class CompactSearchKeyboard : SearchKeyboardBase
     {
-        protected virtual bool ShowFilterButton => true;
-
         protected override Vector2 DefaultKeySize => new Vector2(7f, 7f);
         protected override float DefaultFontSize => 3.5f;
+        protected virtual float YOffset => 5f;
         public bool SymbolButtonInteractivity
         {
             get
@@ -27,10 +26,10 @@ namespace EnhancedSearchAndFilters.UI.Components
             }
         }
 
-        private bool _symbolModeActive = false;
+        protected bool _symbolModeActive = false;
 
-        private TextMeshProButton _symbolButton;
-        private TextMeshProButton _filterButton;
+        protected TextMeshProButton _symbolButton;
+        protected TextMeshProButton _filterButton;
 
         /// <summary>
         /// Indices 0-25 => a to z, indices 26-35 => 0 to 9
@@ -42,14 +41,15 @@ namespace EnhancedSearchAndFilters.UI.Components
             // work under the assumption that the box we can draw on is of size 80u x 60u
             TextMeshProButton button;
             Vector2 anchoredPos;
+            float keySize = DefaultKeySize.x + 1f;
             for (int i = 0; i < KeyArray.Length; ++i)
             {
                 if (i < 10)
-                    anchoredPos = new Vector2(i * 8f, 29f);
+                    anchoredPos = new Vector2(i * keySize, keySize * 3 + YOffset);
                 else if (i < 19)
-                    anchoredPos = new Vector2((i - 10) * 8f + 4f, 21f);
+                    anchoredPos = new Vector2((i - 10) * keySize + keySize / 2f, keySize * 2 + YOffset);
                 else
-                    anchoredPos = new Vector2((i - 19) * 8f + 8f, 13f);
+                    anchoredPos = new Vector2((i - 19) * keySize + keySize, keySize + YOffset);
 
                 button = CreateKeyboardButton(anchoredPos, KeyArray[i].ToUpper());
 
@@ -76,29 +76,52 @@ namespace EnhancedSearchAndFilters.UI.Components
                 });
             }
 
+            CreateBottomButtons();
+
+            // Numbers
+            for (int i = 1; i <= 10; ++i)
+            {
+                button = CreateKeyboardButton(new Vector2((i - 1) * keySize, keySize * 4 + YOffset), i.ToString());
+
+                string key = i.ToString().Last().ToString();
+                string symbol = NumberSymbolArray[i - 1];
+                int index = (i % 10) + 26;
+
+                _keyPairs[index].key = key;
+                _keyPairs[index].symKey = symbol;
+                _keyPairs[index].button = button;
+
+                button.text.text = key;
+                button.button.onClick.AddListener(delegate ()
+                {
+                    if (_symbolModeActive)
+                        InvokeTextButtonPressed(symbol[0]);
+                    else
+                        InvokeTextButtonPressed(key[0]);
+                });
+            }
+        }
+
+        protected virtual void CreateBottomButtons()
+        {
             // Backspace
-            button = CreateKeyboardButton(new Vector2(64f, 13f), new Vector2(15f, 7f), "Backspace");
+            TextMeshProButton button = CreateKeyboardButton(new Vector2(64f, YOffset + DefaultKeySize.y + 1f), new Vector2(15f, DefaultKeySize.y), "Backspace");
             button.text.text = "<-";
             button.button.onClick.AddListener(InvokeDeleteButtonPressed);
 
-            // Space bar
-            button = CreateKeyboardButton(new Vector2(15.5f, 5f), new Vector2(33f, 7f), "Spacebar");
-            button.text.text = "Space";
-            button.button.onClick.AddListener(() => InvokeTextButtonPressed(' '));
-
             // Symbols
-            _symbolButton = CreateKeyboardButton(new Vector2(0f, 5f), new Vector2(15f, 7f), "Symbols");
+            _symbolButton = CreateKeyboardButton(new Vector2(0f, YOffset), new Vector2(15f, DefaultKeySize.y), "Symbols");
             _symbolButton.text.text = "Symbols";
             _symbolButton.text.fontSize = 3f;
             _symbolButton.button.onClick.AddListener(() => SetSymbolMode(!_symbolModeActive));
 
-            // Clear
-            button = CreateKeyboardButton(new Vector2(66f, 5f), new Vector2(13f, 7f), "Clear");
-            button.text.text = "Clear";
-            button.button.onClick.AddListener(InvokeClearButtonPressed);
+            // Space bar
+            button = CreateKeyboardButton(new Vector2(16f, YOffset), new Vector2(32f, DefaultKeySize.y), "Spacebar");
+            button.text.text = "Space";
+            button.button.onClick.AddListener(() => InvokeTextButtonPressed(' '));
 
             // To Filter key
-            _filterButton = CreateKeyboardButton(new Vector2(49.25f, 5f), new Vector2(16f, 7f), "To Filter");
+            _filterButton = CreateKeyboardButton(new Vector2(49f, YOffset), new Vector2(16f, DefaultKeySize.y), "To Filter");
             _filterButton.text.text = "To\nFilter";
             _filterButton.text.fontSize = 2.6f;
 
@@ -134,28 +157,10 @@ namespace EnhancedSearchAndFilters.UI.Components
                 }
             }
 
-            // Numbers
-            for (int i = 1; i <= 10; ++i)
-            {
-                button = CreateKeyboardButton(new Vector2((i - 1) * 8f, 37f), i.ToString());
-
-                string key = i.ToString().Last().ToString();
-                string symbol = NumberSymbolArray[i - 1];
-                int index = (i % 10) + 26;
-
-                _keyPairs[index].key = key;
-                _keyPairs[index].symKey = symbol;
-                _keyPairs[index].button = button;
-
-                button.text.text = key;
-                button.button.onClick.AddListener(delegate ()
-                {
-                    if (_symbolModeActive)
-                        InvokeTextButtonPressed(symbol[0]);
-                    else
-                        InvokeTextButtonPressed(key[0]);
-                });
-            }
+            // Clear
+            button = CreateKeyboardButton(new Vector2(66f, 5f), new Vector2(13f, 7f), "Clear");
+            button.text.text = "Clear";
+            button.button.onClick.AddListener(InvokeClearButtonPressed);
         }
 
         private void OnDestroy()
@@ -209,6 +214,31 @@ namespace EnhancedSearchAndFilters.UI.Components
 
     internal class SearchFilterKeyboard : CompactSearchKeyboard
     {
-        protected override bool ShowFilterButton => false;
+        protected override Vector2 DefaultKeySize => new Vector2(7.5f, 7.5f);
+        protected override float YOffset => 0f;
+
+        protected override void CreateBottomButtons()
+        {
+            // Backspace
+            TextMeshProButton button = CreateKeyboardButton(new Vector2(68f, YOffset + DefaultKeySize.y + 1f), new Vector2(16f, DefaultKeySize.y), "Backspace");
+            button.text.text = "<-";
+            button.button.onClick.AddListener(InvokeDeleteButtonPressed);
+
+            // Symbols
+            _symbolButton = CreateKeyboardButton(new Vector2(0f, YOffset), new Vector2(16f, DefaultKeySize.y), "Symbols");
+            _symbolButton.text.text = "Symbols";
+            _symbolButton.text.fontSize = 3f;
+            _symbolButton.button.onClick.AddListener(() => SetSymbolMode(!_symbolModeActive));
+
+            // Space bar
+            button = CreateKeyboardButton(new Vector2(17f, YOffset), new Vector2(51f, DefaultKeySize.y), "Spacebar");
+            button.text.text = "Space";
+            button.button.onClick.AddListener(() => InvokeTextButtonPressed(' '));
+
+            // Clear
+            button = CreateKeyboardButton(new Vector2(69f, YOffset), new Vector2(16f, DefaultKeySize.y), "Clear");
+            button.text.text = "Clear";
+            button.button.onClick.AddListener(InvokeClearButtonPressed);
+        }
     }
 }
