chrislee0419/EnhancedSearchAndFilters

commit cf4321dc921cca0898d4da4e7be74e3bcd62d6a2

fixes search for playlists
- also some changes how the sorted level pack works
- should resolve #14

UI/SongListUI.cs
@@ -285,8 +285,7 @@ namespace EnhancedSearchAndFilters.UI
                 _searchFlowCoordinator.SongSelected += SelectSongFromSearchResult;
             }
 
-            IBeatmapLevelPack levelPack = LevelSelectionNavigationController.GetPrivateField<IBeatmapLevelPack>("_levelPack");
-            _searchFlowCoordinator.Activate(_freePlayFlowCoordinator, levelPack);
+            _searchFlowCoordinator.Activate(_freePlayFlowCoordinator, _lastPack);
 
             if (!ButtonPanel.IsSingletonAvailable || !ButtonPanel.instance.Initialized)
                 Logger.log.Debug("'Search' button pressed.");
@@ -373,27 +372,20 @@ namespace EnhancedSearchAndFilters.UI
 
             if (_lastPack != null)
             {
-                if (_lastPack is IBeatmapLevelPack levelPack)
-                {
-                    LevelSelectionNavigationController.SetData(_sortedLevelsLevelPack.SetupFromLevelPack(levelPack),
-                        true,
-                        LevelSelectionNavigationController.GetPrivateField<bool>("_showPlayerStatsInDetailView"),
-                        LevelSelectionNavigationController.GetPrivateField<bool>("_showPracticeButtonInDetailView"));
-                }
-                else if (!SongSortModule.IsDefaultSort)
-                {
-                    LevelSelectionNavigationController.SetData(_sortedLevelsLevelPack.SetupFromLevels(_lastPack.beatmapLevelCollection.beatmapLevels),
-                        true,
-                        LevelSelectionNavigationController.GetPrivateField<bool>("_showPlayerStatsInDetailView"),
-                        LevelSelectionNavigationController.GetPrivateField<bool>("_showPracticeButtonInDetailView"));
-                }
-                else
+                if (SongSortModule.IsDefaultSort)
                 {
                     LevelSelectionNavigationController.SetData(_lastPack,
                         true,
                         LevelSelectionNavigationController.GetPrivateField<bool>("_showPlayerStatsInDetailView"),
                         LevelSelectionNavigationController.GetPrivateField<bool>("_showPracticeButtonInDetailView"),
                         null);
+                }
+                else
+                {
+                    LevelSelectionNavigationController.SetData(_sortedLevelsLevelPack.SetupFromLevelCollection(_lastPack),
+                        true,
+                        LevelSelectionNavigationController.GetPrivateField<bool>("_showPlayerStatsInDetailView"),
+                        LevelSelectionNavigationController.GetPrivateField<bool>("_showPracticeButtonInDetailView"));
                 }
             }
             else
@@ -418,44 +410,53 @@ namespace EnhancedSearchAndFilters.UI
                     LevelSelectionNavigationController.GetPrivateField<bool>("_showPlayerStatsInDetailView"),
                     LevelSelectionNavigationController.GetPrivateField<bool>("_showPracticeButtonInDetailView"));
             }
-            else
+            else if (_lastPack != null)
             {
-                if (_lastPack != null && _lastPack is IBeatmapLevelPack beatmapLevelPack)
+                if (SongSortModule.IsDefaultSort)
                 {
                     LevelSelectionNavigationController.SetData(
-                        _sortedLevelsLevelPack.SetupFromLevelPack(beatmapLevelPack),
+                        _lastPack,
+                        true,
+                        LevelSelectionNavigationController.GetPrivateField<bool>("_showPlayerStatsInDetailView"),
+                        LevelSelectionNavigationController.GetPrivateField<bool>("_showPracticeButtonInDetailView"),
+                        null);
+                }
+                else
+                {
+                    LevelSelectionNavigationController.SetData(
+                        _sortedLevelsLevelPack.SetupFromLevelCollection(_lastPack),
                         true,
                         LevelSelectionNavigationController.GetPrivateField<bool>("_showPlayerStatsInDetailView"),
                         LevelSelectionNavigationController.GetPrivateField<bool>("_showPracticeButtonInDetailView"));
                 }
-                else
+            }
+            // this should pretty much never run (_lastPack should always be non-null)
+            else
+            {
+                var lastLevels = _levelCollectionTableView.GetPrivateField<IPreviewBeatmapLevel[]>("_previewBeatmapLevels");
+                if (lastLevels != null)
                 {
-                    var lastLevels = _levelCollectionTableView.GetPrivateField<IPreviewBeatmapLevel[]>("_previewBeatmapLevels");
-
-                    if (lastLevels != null)
+                    // if using default sort on a playlist, just show it as a playlist instead of creating a new level pack
+                    if (SongSortModule.IsDefaultSort)
                     {
-                        // if using default sort on a playlist, just show it as a playlist instead of creating a new level pack
-                        if (SongSortModule.IsDefaultSort)
-                        {
-                            LevelSelectionNavigationController.SetData(
-                                new BeatmapLevelCollection(lastLevels),
-                                LevelSelectionNavigationController.GetPrivateField<bool>("_showPlayerStatsInDetailView"),
-                                LevelSelectionNavigationController.GetPrivateField<bool>("_showPracticeButtonInDetailView"),
-                                null);
-                        }
-                        else
-                        {
-                            LevelSelectionNavigationController.SetData(
-                                _sortedLevelsLevelPack.SetupFromLevels(lastLevels),
-                                true,
-                                LevelSelectionNavigationController.GetPrivateField<bool>("_showPlayerStatsInDetailView"),
-                                LevelSelectionNavigationController.GetPrivateField<bool>("_showPracticeButtonInDetailView"));
-                        }
+                        LevelSelectionNavigationController.SetData(
+                            new BeatmapLevelCollection(lastLevels),
+                            LevelSelectionNavigationController.GetPrivateField<bool>("_showPlayerStatsInDetailView"),
+                            LevelSelectionNavigationController.GetPrivateField<bool>("_showPracticeButtonInDetailView"),
+                            null);
                     }
                     else
                     {
-                        Logger.log.Warn("Unable to find songs to sort");
+                        LevelSelectionNavigationController.SetData(
+                            _sortedLevelsLevelPack.SetupFromLevels(lastLevels),
+                            true,
+                            LevelSelectionNavigationController.GetPrivateField<bool>("_showPlayerStatsInDetailView"),
+                            LevelSelectionNavigationController.GetPrivateField<bool>("_showPracticeButtonInDetailView"));
                     }
+                }
+                else
+                {
+                    Logger.log.Warn("Unable to find songs to sort");
                 }
             }
         }
@@ -592,10 +593,7 @@ namespace EnhancedSearchAndFilters.UI
                 // reapply sort mode
                 if (!SongSortModule.IsDefaultSort)
                 {
-                    if (levelPack is IBeatmapLevelPack beatmapLevelPack2)
-                        _sortedLevelsLevelPack.SetupFromLevelPack(beatmapLevelPack2);
-                    else
-                        _sortedLevelsLevelPack.SetupFromLevels(levelPack.beatmapLevelCollection.beatmapLevels);
+                    _sortedLevelsLevelPack.SetupFromLevelCollection(levelPack);
 
                     // since the level selection navigation controller shows a level pack using the same event that calls this function
                     // and it technically isn't a guarantee that this function will run after it is set,
@@ -684,12 +682,17 @@ namespace EnhancedSearchAndFilters.UI
             }
             else
             {
-                if (_lastPack is IBeatmapLevelPack levelPack)
-                    _levelsToApply = _sortedLevelsLevelPack.SetupFromLevelPack(levelPack);
-                else if (_lastPack != null)
-                    _levelsToApply = _sortedLevelsLevelPack.SetupFromLevels(_lastPack.beatmapLevelCollection.beatmapLevels);
+                if (_lastPack != null)
+                {
+                    if (!SongSortModule.IsDefaultSort)
+                        _levelsToApply = _sortedLevelsLevelPack.SetupFromLevelCollection(_lastPack);
+                    else
+                        _levelsToApply = _lastPack;
+                }
                 else        // this should never happen (_lastPack should always be defined by this point)
+                {
                     Logger.log.Warn("Unable to unapply filters (could not find previous level pack)");
+                }
 
                 ButtonPanel.instance.SetFilterStatus(false);
             }

Search/WordCountStorage.cs
@@ -24,7 +24,7 @@ namespace EnhancedSearchAndFilters.Search
         public WordCountStorage()
         { }
 
-        public WordCountStorage(IBeatmapLevelPack levelPack)
+        public WordCountStorage(IAnnotatedBeatmapLevelCollection levelPack)
         {
             SetupStorage(levelPack);
         }
@@ -33,7 +33,7 @@ namespace EnhancedSearchAndFilters.Search
         /// Populate word storage with the words in the song name, sub-name, author, and map creator of a level pack.
         /// </summary>
         /// <param name="levelPack">The level pack whose words you want to store.</param>
-        public void SetupStorage(IBeatmapLevelPack levelPack)
+        public void SetupStorage(IAnnotatedBeatmapLevelCollection levelPack)
         {
             IsLoading = true;
             _manualResetEvent = new ManualResetEvent(true);
@@ -43,13 +43,13 @@ namespace EnhancedSearchAndFilters.Search
                 delegate ()
                 {
                     var sw = System.Diagnostics.Stopwatch.StartNew();
-                    Logger.log.Info($"Creating word count storage object for the \"{levelPack.packName}\" level pack (contains {levelPack.beatmapLevelCollection.beatmapLevels.Length} songs)");
+                    Logger.log.Info($"Creating word count storage object for the \"{levelPack.collectionName}\" level pack (contains {levelPack.beatmapLevelCollection.beatmapLevels.Length} songs)");
 
                     if (!SetWordsFromLevelPack(levelPack))
                         return;
 
                     sw.Stop();
-                    Logger.log.Info($"Finished creating word count storage object for the \"{levelPack.packName}\" level pack (took {sw.ElapsedMilliseconds/1000f} seconds, {_words.Count} unique words processed)");
+                    Logger.log.Info($"Finished creating word count storage object for the \"{levelPack.collectionName}\" level pack (took {sw.ElapsedMilliseconds/1000f} seconds, {_words.Count} unique words processed)");
                 },
                 delegate ()
                 {
@@ -98,7 +98,7 @@ namespace EnhancedSearchAndFilters.Search
             Logger.log.Info("Cancelling word count storage setup thread");
         }
 
-        private bool SetWordsFromLevelPack(IBeatmapLevelPack levelPack)
+        private bool SetWordsFromLevelPack(IAnnotatedBeatmapLevelCollection levelPack)
         {
             // we don't build the _words object immediately only because we want to also add the
             // counts of other words that are prefixed by a word to the count

Search/WordPredictionEngine.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Linq;
 using EnhancedSearchAndFilters.Filters;
+using EnhancedSearchAndFilters.SongData;
+using EnhancedSearchAndFilters.Tweaks;
 
 namespace EnhancedSearchAndFilters.Search
 {
@@ -16,20 +18,28 @@ namespace EnhancedSearchAndFilters.Search
         public static readonly char[] SpaceCharArray = new char[] { ' ' };
 
         public const int SuggestedWordsCountThreshold = 10;
+        public const string BuiltInFavouritesPackCollectionName = "Favorites";
 
         private WordPredictionEngine()
         { }
 
-        public void SetActiveWordStorageFromLevelPack(IBeatmapLevelPack levelPack)
+        public void SetActiveWordStorageFromLevelPack(IAnnotatedBeatmapLevelCollection levelCollection)
         {
-            if (!_cache.TryGetValue(levelPack.packName, out var storage))
+            if (!_cache.TryGetValue(levelCollection.collectionName, out var storage))
             {
-                storage = new WordCountStorage(levelPack);
+                storage = new WordCountStorage(levelCollection);
 
-                // never cache filtered level packs
-                if (!(levelPack.packName == FilteredLevelsLevelPack.PackName) &&
-                    !Tweaks.SongBrowserTweaks.IsFilterApplied())
-                    _cache[levelPack.packName] = storage;
+                string collectionName = levelCollection.collectionName;
+
+                // never cache filtered/built-in favorites level packs
+                if (collectionName != FilteredLevelsLevelPack.CollectionName &&
+                    collectionName != BuiltInFavouritesPackCollectionName &&
+                    collectionName != BuiltInFavouritesPackCollectionName + SortedLevelsLevelPack.PackIDSuffix &&
+                    collectionName != SortedLevelsLevelPack.PackIDSuffix &&
+                    !SongBrowserTweaks.IsFilterApplied())
+                    _cache[levelCollection.collectionName.Replace(SortedLevelsLevelPack.PackIDSuffix, "")] = storage;
+                else
+                    Logger.log.Warn($"not storing '{levelCollection.collectionName}' collection in wordpredictionengine");
             }
 
             _activeWordStorage = storage;

SongData/SortedLevelsLevelPack.cs
@@ -21,21 +21,41 @@ namespace EnhancedSearchAndFilters.SongData
         public const string PackName = "Sorted Songs";
 
         /// <summary>
-        /// Applies and stores sorting of a provided IBeatmapLevelPack.
+        /// Applies and stores sorting of a provided <see cref="IBeatmapLevelPack"/>.
         /// </summary>
         /// <param name="levelPack">A level pack to sort.</param>
         /// <returns>Returns itself if not using the non-default sort mode, otherwise returns the provided level pack.</returns>
-        public IBeatmapLevelPack SetupFromLevelPack(IBeatmapLevelPack levelPack)
+        private IBeatmapLevelPack SetupFromLevelPack(IBeatmapLevelPack levelPack)
         {
             if (SongSortModule.IsDefaultSort)
                 return levelPack;
 
             packID = levelPack.packID + PackIDSuffix;
             packName = levelPack.packName;
-            shortPackName = levelPack.shortPackName;
+            shortPackName = levelPack.shortPackName + PackIDSuffix;
             coverImage = levelPack.coverImage;
 
             _beatmapLevelCollection.SetPrivateField("_levels", SongSortModule.SortSongs(levelPack.beatmapLevelCollection.beatmapLevels), typeof(BeatmapLevelCollection));
+
+            return this;
+        }
+
+        /// <summary>
+        /// Applies and stores sorting of a provided <see cref="IAnnotatedBeatmapLevelCollection"/>.
+        /// </summary>
+        /// <param name="levelCollection">A level collection to sort.</param>
+        /// <returns>Returns itself if not using the non-default sort mode, otherwise returns the provided level pack.</returns>
+        public IBeatmapLevelPack SetupFromLevelCollection(IAnnotatedBeatmapLevelCollection levelCollection)
+        {
+            if (levelCollection is IBeatmapLevelPack levelPack)
+                return SetupFromLevelPack(levelPack);
+
+            packID = PackIDSuffix;
+            packName = PackName;
+            shortPackName = levelCollection.collectionName + PackIDSuffix;
+            coverImage = levelCollection.coverImage;
+
+            _beatmapLevelCollection.SetPrivateField("_levels", SongSortModule.SortSongs(levelCollection.beatmapLevelCollection.beatmapLevels), typeof(BeatmapLevelCollection));
 
             return this;
         }

UI/FlowCoordinators/SearchFlowCoordinator.cs
@@ -122,7 +122,7 @@ namespace EnhancedSearchAndFilters.UI.FlowCoordinators
         /// </summary>
         /// <param name="parentFlowCoordinator">The flow coordinator that will be immediately higher in the hierarchy that will present this flow coordinator.</param>
         /// <param name="levels">The list of levels that will be used as the search space.</param>
-        public void Activate(FlowCoordinator parentFlowCoordinator, IBeatmapLevelPack levelPack)
+        public void Activate(FlowCoordinator parentFlowCoordinator, IAnnotatedBeatmapLevelCollection levelPack)
         {
             _levelsSearchSpace = levelPack.beatmapLevelCollection.beatmapLevels;
             Action onFinish = PushInitialViewControllersToNavigationController;

commit e29c8f94af8eb3141125eefe8d201058117daae5

fixes search failing with SongBrowser
- fixes the bug introduced in cf4321dc where the _lastPack passed to the
  WordPredictionEngine is null because _lastPack was no longer being updated
  when SongBrowser was present

UI/SongListUI.cs
@@ -85,30 +85,22 @@ namespace EnhancedSearchAndFilters.UI
                 _freePlayFlowCoordinator = FindObjectOfType<SoloFreePlayFlowCoordinator>();
                 (_freePlayFlowCoordinator as SoloFreePlayFlowCoordinator).didFinishEvent += OnFreePlayFlowCoordinatorFinished;
 
+                PrepareLevelPackSelectedEvent();
                 if (!SongBrowserTweaks.ModLoaded)
-                {
-                    PrepareLevelPackSelectedEvent();
                     StartCoroutine(UIUtilities.DelayedAction(SelectSavedLevelPack));
-                }
                 else if (!SongBrowserTweaks.Initialized)
-                {
                     StartCoroutine(GetSongBrowserButtons());
-                }
             }
             else if (mode == FreePlayMode.Party)
             {
                 _freePlayFlowCoordinator = FindObjectOfType<PartyFreePlayFlowCoordinator>();
                 (_freePlayFlowCoordinator as PartyFreePlayFlowCoordinator).didFinishEvent += OnFreePlayFlowCoordinatorFinished;
 
+                PrepareLevelPackSelectedEvent();
                 if (!SongBrowserTweaks.ModLoaded)
-                {
-                    PrepareLevelPackSelectedEvent();
                     StartCoroutine(UIUtilities.DelayedAction(SelectSavedLevelPack));
-                }
                 else if (!SongBrowserTweaks.Initialized)
-                {
                     StartCoroutine(GetSongBrowserButtons());
-                }
             }
             else if (mode == FreePlayMode.Campaign)
             {
@@ -121,8 +113,8 @@ namespace EnhancedSearchAndFilters.UI
 
         private void PrepareLevelPackSelectedEvent()
         {
-            // we don't need the LevelPackSelected event if using SongBrowser, since it passes the songs it needs filtered
-            // directly to us (so we don't need to store last pack at all)
+            // we still need this delegate when using SongBrowser, since the WordPredictionEngine now uses IAnnotatedBeatmapLevelCollections,
+            // which aren't stored by the LevelFilteringNavigationController
             _isSelectingInitialLevelPack = true;
             LevelFilteringNavigationController = _freePlayFlowCoordinator.GetPrivateField<LevelFilteringNavigationController>("_levelFilteringNavigationController", typeof(LevelSelectionFlowCoordinator));
             LevelFilteringNavigationController.didSelectAnnotatedBeatmapLevelCollectionEvent -= LevelPackSelected;
@@ -561,13 +553,19 @@ namespace EnhancedSearchAndFilters.UI
                 _isSelectingInitialLevelPack = false;
                 return;
             }
-
             // in ConfirmDeleteButtonClicked, the call to SongCore.Loader.Instance.DeleteSong will reload the level packs
             // which causes the custom level pack to be re-selected. but, if filters are applied or level pack is sorted,
             // we want to reshow our own filtered/sorted level pack and not reset our UI, so we don't have to handle this event
             // this code is kinda smelly tbh, but can't do anything about it unless there are changes to SongCore
-            if (_isDeletingSongInModOwnedLevelPack)
+            else if (_isDeletingSongInModOwnedLevelPack)
+            {
                 return;
+            }
+            // when SongBrowser is enabled, we only need to store the pack for the WordPredictionEngine when using the search feature
+            else if (SongBrowserTweaks.Initialized)
+            {
+                _lastPack = levelPack;
+            }
 
             if (levelPack.collectionName != FilteredLevelsLevelPack.CollectionName)
             {